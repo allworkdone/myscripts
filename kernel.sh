846e34b4f889cef6453a435b52d74c643de38f78
HellVikings <beastdark704@gmail.com>
2022-06-25 21:23:14 +0530

    kernel : Beautify it
    Co-authored-by: UsiFX <xprjkts@gmail.com>
    Signed-off-by: HellVikings <beastdark704@gmail.com>

--- kernel.sh
+++ kernel.sh
@@ -8,7 +8,7 @@
 #
 
 # Set environment for directory
-KERNEL_DIR=$PWD
+KERNEL_DIR=$(pwd)
 IMG_DIR=$KERNEL_DIR/out/arch/arm64/boot
 
 # Get defconfig file
@@ -22,8 +22,8 @@ export KBUILD_BUILD_USER="RealAkira"
 export KBUILD_BUILD_HOST="Arch-Linux"
 
 # Set environment for telegram
-export CHATID="-1001542481275"
-export token="your token"
+export CHATID=""
+export token=""
 export BOT_MSG_URL="https://api.telegram.org/bot$token/sendMessage"
 export BOT_BUILD_URL="https://api.telegram.org/bot$token/sendDocument"
 
@@ -69,7 +69,7 @@ tg_post_build() {
 	-F chat_id="$CHATID"  \
 	-F "disable_web_page_preview=true" \
 	-F "parse_mode=html" \
-	-F caption="$2" # | <b>MD5 Checksum : </b><code>$MD5CHECK</code>"
+	-F caption="$2"
 }
 
 # Set function for cloning repository
@@ -79,7 +79,7 @@ clone() {
 
 	if [[ $COMPILER == "clang" ]]; then
 		# Clone STRIX Clang
-		git clone --depth=1 https://gitlab.com/fiqri19102002/strix-clang -b lto-pgo/14.x clang
+		git clone --depth=1 https://github.com/kdrag0n/proton-clang.git clang
 		# Set environment for clang
 		TC_DIR=$KERNEL_DIR/clang
 		# Get path and compiler string
@@ -92,18 +92,16 @@ clone() {
 
 # Set function for naming zip file
 set_naming() {
-	KERNEL_NAME="liquid-laurel_sprout-personal-$DATE"
-	export ZIP_NAME="$KERNEL_NAME.zip"
+	KERNEL_NAME="liquid-laurel_sprout-personal-${DATE}"
+	export ZIP_NAME=$KERNEL_NAME.zip
 }
 # Set function for zipping into a flashable zip
 gen_zip() {
-        # Move kernel image to AnyKernel3
-	cd AnyKernel3
-        cp $IMG_DIR/Image.gz-dtb $(pwd)
-#        cd AnyKernel3
+		cd AnyKernel3
+        cp $IMG_DIR/Image.gz-dtb AnyKernel3
 
         # Archive to flashable zip
-        zip -r9 "$ZIP_NAME" *
+        zip -r9 $ZIP_NAME *
 
         # Prepare a final zip variable
         ZIP_FINAL="$ZIP_NAME"
@@ -114,7 +112,7 @@ gen_zip() {
 # Set function for starting compile
 compile() {
 	echo -e "Kernel compilation starting"
-	tg_post_msg "<b>• liquid° Kernel •</b>%0Astarted on: <code>liquidCI</code>%0Adevice: <b>Mi A3</b>%0Abranch: <code>$(git rev-parse --abbrev-ref HEAD)</code>%0Acompiler: <code>${KBUILD_COMPILER_STRING}</code>%0Astart date: <code>$(date)</code>%0A<b>Status : </b>#Personal"
+	tg_post_msg "<b>Docker OS: </b><code>$DISTRO</code>%0A<b>Kernel Version : </b><code>$KERVER</code>%0A<b>Date : </b><code>$(TZ=Asia/Kolkata date)</code>%0A<b>Device : </b><code>MI A3 (laurel_sprout)</code>%0A<b>Pipeline Host : </b><code>$KBUILD_BUILD_HOST</code>%0A<b>Host Core Count : </b><code>$PROCS</code>%0A<b>Compiler Used : </b><code>$KBUILD_COMPILER_STRING</code>%0a<b>Branch : </b><code>$BRANCH</code>%0A<b>Last Commit : </b><code>$COMMIT_HEAD</code>%0A<b>Status : </b>#Personal"
 	make O=out "$DEFCONFIG"
 	BUILD_START=$(date +"%s")
 	if [[ $COMPILER == "clang" ]]; then
