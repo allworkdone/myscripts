fac265331ffc2e2dfa11bad191a7b5481a78fdfa
UsiFX <xprjkts@gmail.com>
2022-07-31 11:28:45 +0530

    kernel : Cleanup and organise
    Signed-off-by: Hellvikings <beastdark704@gmail.com>

--- kernel.sh
+++ kernel.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/bin/bash
 
 #
 # Script for building Android arm64 Kernel
@@ -6,6 +6,11 @@
 # Copyright (c) 2022 RealAkira <beastdark704@gmail.com>
 # Based on Panchajanya1999 script.
 #
+
+# \e colors
+BLUE="\e[1;35m"
+RED="\e[1;31m"
+GREEN="\e[1;32m"
 
 # Set environment for directory
 KERNEL_DIR=$(pwd)
@@ -19,11 +24,11 @@ export ARCH=arm64
 export SUBARCH=arm64
 export KBUILD_BUILD_VERSION="1"
 export KBUILD_BUILD_USER="RealAkira"
-export KBUILD_BUILD_HOST="Arch-Linux"
+export KBUILD_BUILD_HOST="ZorinOS"
 
 # Set environment for telegram
-export CHATID=""
-export token=""
+export CHATID="-1001542481275"
+export token="5389275341:AAFtB8oBu3KUO2_EY68XwQ-mEwBXPOEp64A"
 export BOT_MSG_URL="https://api.telegram.org/bot$token/sendMessage"
 export BOT_BUILD_URL="https://api.telegram.org/bot$token/sendDocument"
 
@@ -32,19 +37,14 @@ export BOT_BUILD_URL="https://api.telegram.org/bot$token/sendDocument"
 #
 COMPILER=clang
 
-# Get distro name
-DISTRO=$(source /etc/os-release && echo ${NAME})
-
 # Get all cores of CPU
-PROCS=$(nproc --all)
-export PROCS
+export PROCS=$(nproc --all)
 
 # Set Date and time
-DATE=$(TZ=Asia/Kolkata date +"%Y%m%d-%T")
+DATE=$(TZ=Asia/Kolkata date +"%Y%m%d")
 
 # Get branch name
-BRANCH=$(git rev-parse --abbrev-ref HEAD)
-export BRANCH
+export BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
 # Check kernel version
 KERVER=$(make kernelversion)
@@ -53,18 +53,16 @@ KERVER=$(make kernelversion)
 COMMIT_HEAD=$(git log --oneline -1)
 
 # Set function for telegram
-tg_post_msg() {
+tg_post_msg()
+{
 	curl -s -X POST "$BOT_MSG_URL" -d chat_id="$CHATID" \
-	-d "disable_web_page_preview=true" \
-	-d "parse_mode=html" \
-	-d text="$1"
+		-d "disable_web_page_preview=true" \
+		-d "parse_mode=html" \
+		-d text="$1"
 }
 
-tg_post_build() {
-	# Post MD5 Checksum alongwith for easeness
-#	MD5CHECK=$(md5sum "$1" | cut -d' ' -f1)
-
-	# Show the Checksum alongwith caption
+tg_post_build()
+{
 	curl --progress-bar -F document=@"$1" "$BOT_BUILD_URL" \
 	-F chat_id="$CHATID"  \
 	-F "disable_web_page_preview=true" \
@@ -73,32 +71,36 @@ tg_post_build() {
 }
 
 # Set function for cloning repository
-clone() {
+clone()
+{
+	rm -rf AnyKernel3
 	# Clone AnyKernel3
 	git clone --depth=1 https://github.com/RealAkira/AnyKernel3.git AnyKernel3
 
-	if [[ $COMPILER == "clang" ]]; then
-		# Clone STRIX Clang
-		git clone --depth=1 https://github.com/kdrag0n/proton-clang.git clang
-		# Set environment for clang
-		TC_DIR=$KERNEL_DIR/clang
-		# Get path and compiler string
-		KBUILD_COMPILER_STRING=$("$TC_DIR"/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
-		PATH=$TC_DIR/bin/:$PATH
-	fi
+	# Clone Clang
+	git clone --depth=1 https://github.com/kdrag0n/proton-clang.git clang
 
-	export PATH KBUILD_COMPILER_STRING
+	# Set environment for clang
+	TC_DIR=$KERNEL_DIR/clang
+
+	# Get path and compiler string
+	export KBUILD_COMPILER_STRING=$("$TC_DIR"/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
+	export PATH=$TC_DIR/bin/:$PATH
 }
 
 # Set function for naming zip file
-set_naming() {
+set_naming()
+{
 	KERNEL_NAME="liquid-laurel_sprout-personal-${DATE}"
 	export ZIP_NAME=$KERNEL_NAME.zip
 }
+
 # Set function for zipping into a flashable zip
-gen_zip() {
-		cd AnyKernel3
-        cp $IMG_DIR/Image.gz-dtb AnyKernel3
+gen_zip()
+{
+	cd $(pwd)/AnyKernel3
+        cp $IMG_DIR/Image.gz Image.gz
+	cp $IMG_DIR/dtbo.img dtbo.img
 
         # Archive to flashable zip
         zip -r9 $ZIP_NAME *
@@ -107,12 +109,13 @@ gen_zip() {
         ZIP_FINAL="$ZIP_NAME"
 
         tg_post_build $ZIP_FINAL "Build finished"
-        cd ..
+	cd ..
 }
 # Set function for starting compile
-compile() {
-	echo -e "Kernel compilation starting"
-	tg_post_msg "<b>Docker OS: </b><code>$DISTRO</code>%0A<b>Kernel Version : </b><code>$KERVER</code>%0A<b>Date : </b><code>$(TZ=Asia/Kolkata date)</code>%0A<b>Device : </b><code>MI A3 (laurel_sprout)</code>%0A<b>Pipeline Host : </b><code>$KBUILD_BUILD_HOST</code>%0A<b>Host Core Count : </b><code>$PROCS</code>%0A<b>Compiler Used : </b><code>$KBUILD_COMPILER_STRING</code>%0a<b>Branch : </b><code>$BRANCH</code>%0A<b>Last Commit : </b><code>$COMMIT_HEAD</code>%0A<b>Status : </b>#Personal"
+compile()
+{
+	echo -e "${BLUE} Kernel compilation starting\e[0m"
+	tg_post_msg "<b>Kernel Version : </b><code>$KERVER</code>%0A<b>Date : </b><code>$(TZ=Asia/Kolkata date)</code>%0A<b>Device : </b><code>MI A3 (laurel_sprout)</code>%0A<b>Pipeline Host : </b><code>$KBUILD_BUILD_HOST</code>%0A<b>Host Core Count : </b><code>$PROCS</code>%0A<b>Compiler Used : </b><code>$KBUILD_COMPILER_STRING</code>%0a<b>Branch : </b><code>$BRANCH</code>%0A<b>Last Commit : </b><code>$COMMIT_HEAD</code>%0A<b>Status : </b>#Personal"
 	make O=out "$DEFCONFIG"
 	BUILD_START=$(date +"%s")
 	if [[ $COMPILER == "clang" ]]; then
@@ -125,25 +128,20 @@ compile() {
 				LD=ld.lld \
 				OBJDUMP=llvm-objdump \
 				STRIP=llvm-strip
-	elif [[ $COMPILER == "gcc" ]]; then
-		export CROSS_COMPILE_ARM32=$GCC32_DIR/bin/arm-eabi-
-		make -j"$PROCS" O=out CROSS_COMPILE=aarch64-elf-
 	fi
 	BUILD_END=$(date +"%s")
 	DIFF=$((BUILD_END - BUILD_START))
-	if [ -f "$IMG_DIR"/Image.gz-dtb ]
+	if [[ -f "$IMG_DIR"/Image.gz ]]
 	then
-		echo -e "Kernel successfully compiled"
+		echo -e "${GREEN} Kernel successfully compiled\e[0m"
 		gen_zip
-	elif ! [ -f "$IMG_DIR"/Image.gz-dtb ]
+	elif [[ ! -f "$IMG_DIR"/Image.gz ]]
 	then
-		echo -e "Kernel compilation failed"
+		echo -e "${RED} Kernel compilation failed\e[0m"
 		tg_post_msg "<b>Build failed</b>"
 		exit 1
 	fi
 }
-
-
 
 clone
 compile
